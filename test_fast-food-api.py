21ce86a6a4e084b4eb6f2035ded152d597648ab6
Charles <charlesiddle99@gmail.com>
2018-09-06 17:53:48 +0300

    the user can now update the status

--- test_fast-food-api.py
+++ test_fast-food-api.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,json
 #imports pytest
 import pytest
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 #mock_data
 order_items = [{'name':'pizza'},{'name':'steak'},{'name':'rice'},{'name':'Fries'}]
-
+item_to_be_added = [{'name':'ugali'}]
 
 #red test
 def test_orders():
@@ -19,6 +19,23 @@ def test_orders():
 @app.route('/api/v1/orders',methods=['GET'])
 def orders():	
 	return jsonify({'order_items':order_items})
+"""
+tests the update order
+
+"""
+#red test
+def test_update():	
+	#test to see if the order updates
+    result=app.test_client()
+    response= result.put('/api/v1/order', data=json.dumps(item_to_be_added) ,content_type='application/json')
+    assert(response.status_code==404)
+#green
+@app.route('/api/v1/order/<string:name>',methods=['PUT'])
+def update(name):
+	ordered = [item for item in order_items if item['name']==name]
+	ordered[0]['name'] = request.json['name']
+	return jsonify({'item':ordered[0]})
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
