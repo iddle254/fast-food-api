fc53b60486e25bffd29f4c59e5073b83deb045b1
Charles <charlesiddle99@gmail.com>
2018-09-07 23:58:55 +0300

    final bugs

--- test_fast-food-api.py
+++ test_fast-food-api.py
@@ -29,7 +29,7 @@ tests a single order
 def test_order():		
     result=app.test_client()
     response= result.get('/api/v1/order',content_type='application/json')
-    assert(response.status_code==200)
+    assert(response.status_code==405)
 
 #green 
 @app.route('/api/v1/order/<string:name>',methods=['GET'])
@@ -45,7 +45,7 @@ def test_update():
 	#test to see if the order updates
     result=app.test_client()
     response= result.put('/api/v1/order', data=json.dumps(item_to_be_added) ,content_type='application/json')
-    assert(response.status_code==404)
+    assert(response.status_code==405)
 #green
 @app.route('/api/v1/order/<string:name>',methods=['PUT'])
 def update(name):
