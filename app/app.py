99f6395adcd946a02b8ae6567a25dcb45c91dde2
Charles <charlesiddle99@gmail.com>
2018-09-19 15:26:27 +0300

    [Finishes #160599009]

--- app/app.py
+++ app/app.py
@@ -1,23 +1,30 @@
-from flask import Flask,jsonify,json
+from flask import Flask,jsonify,json,request
 
 #creates an instance of flask
 app = Flask(__name__)
 
 #mock_data
-order_items = []		
+order_items = []
+item_to_be_added = {'name':'ugali'}		
 #green
 @app.route('/api/v1/orders',methods=['GET'])
 def orders():	
 	return jsonify({'order_items':order_items})
 
 #green 
-@app.route('/api/v1/order/<string:name>',methods=['GET'])
+@app.route('/api/v1/orders/<string:name>',methods=['GET'])
 def order(name):
 	ordered = [item for item in order_items if item['name']==name]
 	return jsonify({'item':ordered[0]})
 
+@app.route('/api/v1/orders',methods=['POST'])
+def new_order():	     
+	item = {'name':request.json['name']}
+	order_items.append(item)
+	return jsonify({'order_items':order_items})
+
 #green
-@app.route('/api/v1/order/<string:name>',methods=['PUT'])
+@app.route('/api/v1/orders/<string:name>',methods=['PUT'])
 def update(name):
 	ordered = [item for item in order_items if item['name']==name]
 	ordered[0]['name'] = request.json['name']
--- app/test_fast-food-api.py
+++ app/test_fast-food-api.py
@@ -1,9 +1,10 @@
-from flask import Flask,jsonify,json
+from flask import Flask,jsonify,json,request
 #imports pytest
 import pytest
 from app import app
 
-item_to_be_added={"ugali":"choma"}
+order_items=[]
+item_to_be_added = {'name':'ugali'}
 #red testa
 def test_orders():
 	#test to see if orders returns all the orders	
@@ -14,14 +15,18 @@ def test_orders():
 #test to see if only one order is returned
 def test_order():		
     result=app.test_client()
-    response= result.get('/api/v1/order',content_type='application/json')
-    assert(response.status_code==404)
+    response= result.get('/api/v1/orders',content_type='application/json')
+    assert(response.status_code==200)
 
 #red test
-def test_update():	
-	#test to see if the order updates
-    result=app.test_client()
-    response= result.put('/api/v1/order', data=json.dumps(item_to_be_added) ,content_type='application/json')
-    assert(response.status_code==404)
+def test_new_order():
+	#test to see if a new order is created
+	result=app.test_client()
+	no_orders=len(order_items)
+	response= result.post('/api/v1/orders', data=json.dumps(item_to_be_added) ,content_type='application/json')
+	data=json.loads(response.data)
+	no_new_orders = len (data)+no_orders    
+	assert no_orders + 1 == no_new_orders
+	assert(response.status_code==200)
 
 
