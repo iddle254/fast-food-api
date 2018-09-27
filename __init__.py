86f45455cbc8ad32333008ca06e514bfbfc52064
Charles <charlesiddle99@gmail.com>
2018-09-27 18:03:42 +0300

    Added orderId

--- __init__.py
+++ __init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from .views.adminorders import profile
 
-def create_app(app):
+def create_app(config_name):	
     app = Flask(__name__)
     app.register_blueprint(profile)
-    return create_app(app)
+    @app.errorhandler(404)
+    def pageNotFound(e):
+        return "Page not found. Please check to see if you entered the correct address"
+
+    @app.errorhandler(405)
+    def methodNotAllowed(e):
+	    return "Method not allowed. Please use the correct method"
+
+    @app.errorhandler(500)
+    def methodNotAllowed(e):
+	    return "oops.. There is a problem on our part. Please wait as we try to fix it"
+    return app
--- views/adminorders.py
+++ views/adminorders.py
@@ -13,14 +13,14 @@ item_to_be_added = {'name':'ugali'}
 def orders():	
 	return jsonify({'order_items':order_items})
 
-@profile.route('/<string:name>',methods=['GET'])
+@profile.route('/<int:orderId>',methods=['GET'])
 def order(name):
     validFoodName = validate.foodNameValidator(name)
     if validFoodName:
 	    ordered = [item for item in order_items if item['name']==name]	
 	    return jsonify({'item':ordered[0]})
 
-@profile.route('/<string:name>',methods=['PUT'])
+@profile.route('/<int:orderId>',methods=['PUT'])
 def update(name):
 	validFoodName = validate.foodNameValidator(name)
 	if validFoodName:
