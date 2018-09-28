e8cd3552f1b1aff41f0a729f096c6d64fe97a67f
Charles <charlesiddle99@gmail.com>
2018-09-28 03:40:05 +0300

    tests rated at 8.3 [finishes #160825668]

--- app/test_fast-food-api.py
+++ app/test_fast-food-api.py
@@ -1,39 +1,36 @@
-from flask import Flask,jsonify,json,request
+from flask import json
 #imports pytest
-import pytest
 from app import app
 
-order_items=[]
+order_items = []
 item_to_be_added = {'name':'ugali'}
 #red testa
 def test_orders():
-	#test to see if orders returns all the orders	
-    result=app.test_client()
-    response= result.get('/api/v1/orders',content_type='application/json')
-    assert(response.status_code==200)
-    
-
-#test to see if only one order is returned
-def test_order():		
-    result=app.test_client()
-    response= result.get('/api/v1/orders',content_type='application/json')
-    assert(response.status_code==200)
+    """test to see if orders are created"""
+    result = app.test_client()
+    response = result.get('/api/v1/orders', content_type='application/json')
+    assert response.status_code == 200
+def test_order():
+    """test to see if a new order is created"""
+    result = app.test_client()
+    response = result.get('/api/v1/orders', content_type='application/json')
+    assert response.status_code == 200
 
 #red test
 def test_new_order():
-	#test to see if a new order is created
-	result=app.test_client()
-	no_orders=len(order_items)
-	response= result.post('/api/v1/orders', data=json.dumps(item_to_be_added) ,content_type='application/json')
-	data=json.loads(response.data)
-	no_new_orders = len (data)+no_orders    
-	assert no_orders + 1 == no_new_orders
-	assert(response.status_code==200)
+    """test to see if a new order is created"""
+    result = app.test_client()
+    no_orders = len(order_items)
+    response = result.post('/api/v1/orders',\
+    	               data=json.dumps(item_to_be_added), content_type='application/json')
+    data = json.loads(response.data)
+    no_new_orders = len(data)+no_orders
+    assert no_orders + 1 == no_new_orders
+    assert response.status_code == 200
 #red test
-def test_update():	
-	#test to see if the order updates
-    result=app.test_client()
-    response= result.put('/api/v1/order', data=json.dumps(item_to_be_added) ,content_type='application/json')
-    assert(response.status_code==404)
-
-
+def test_update():
+    """test to see if the order updates"""
+    result = app.test_client()
+    response = result.put('/api/v1/order',\
+    	              data=json.dumps(item_to_be_added), content_type='application/json')
+    assert response.status_code == 404
